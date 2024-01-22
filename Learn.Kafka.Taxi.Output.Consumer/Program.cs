a0e9e119faa09edaa298fb8f63c2f333e4399c85
GioManu <giomanu.gtr@gmail.com>
2024-01-22 18:31:39 +0400

    added consumer.exe added output consumer

---
+++ Learn.Kafka.Taxi.Output.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using Learn.Kafka.Taxi.Shared;
+
+Console.WriteLine("Hello From Output Consumer");
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = KafkaSettings.BootstrapServers,
+    GroupId = KafkaSettings.CalcConsumerGroupId,
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    EnableAutoCommit = true,
+    AutoCommitIntervalMs = 101,
+};
+
+var cancellationTokenSource = new CancellationTokenSource();
+var cancellationToken = cancellationTokenSource.Token;
+using var consumer = new VehicleOutputTopicConsumer(consumerConfig, Console.WriteLine);
+consumer.Subscribe(KafkaSettings.OutputTopic);
+
+consumer.Start(cancellationToken);
+
+if (Console.ReadKey().Key == ConsoleKey.Enter)
+{
+    cancellationTokenSource.Cancel();
+}
+
+Console.ReadLine();
---
+++ Learn.Kafka.Taxi.Shared/VehicleOutputTopicConsumer.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+using Learn.Kafka.Taxi.Shared.Interfaces;
+using Learn.Kafka.Taxi.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Learn.Kafka.Taxi.Shared
+{
+    public class VehicleOutputTopicConsumer : IDisposable
+    {
+        private readonly IConsumer<Ignore, string> _consumer;
+        private readonly Action<string> _log;
+
+        public VehicleOutputTopicConsumer(ConsumerConfig config, Action<string> log)
+        {
+            _consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            _log = log;
+        }
+
+        public void Subscribe(string topic)
+        {
+            _consumer.Subscribe(topic);
+        }
+
+        public void Start(CancellationToken cancellationToken)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var result = _consumer.Consume(cancellationToken);
+                        var messageResult = JsonSerializer.Deserialize<VehicleDistanceCalculatedResult>(result.Message.Value);
+                        _log($"INFO: Received Message: {result.Message.Value}");
+                    }
+                    catch (Exception ex)
+                    {
+                        _log($"ERROR: {ex.Message}");
+                    }
+                }
+                _consumer.Close();
+            }, TaskCreationOptions.LongRunning);
+        }
+
+        public void Dispose() => _consumer.Dispose();
+    }
+}
